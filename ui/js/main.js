f2760acc10ebbc43e0fe21f8c34dadbe0c9411fd
schroffl <hannes@holzwarth.me>
2017-05-04 18:20:17 +0200

    VueJS App

---
+++ ui/js/main.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const Vault = chrome.extension.getBackgroundPage().vault; // TODO: Use chrome.runtime.getBackgroundPage (async)
+
+window.onload = function() {
+	window.app = new Vue({
+		'el': '#vault',
+		'data': {
+			'vault': Vault,
+			'wrongPassword': false,
+			'showLock': true,
+			'title': 'Vault',
+			'editing': false
+		},
+		'methods': {
+			'unlock': function(event) {
+				let password = event.target.value,
+					lockEl = this.$el.querySelector('.lock');
+
+				lockEl.addEventListener('animationend', e => {
+					if(e.animationName !== 'open-vault') return;
+
+					event.target.value = '';
+				});
+
+				return this.vault.unlock(password).catch(err => this.wrongPassword = true);
+			},
+			'insertData': function(data) {
+				return sendMessageToActiveTab({ 'action': 'insert', data }).then(window.close);
+			},
+			'editTray': function(tray) {
+				console.log(tray);
+
+				this.editing = true;				
+			}
+		}
+	});
+};
+
+function sendMessageToActiveTab(message) {
+	return new Promise((resolve, reject) => {
+		getActiveTab().then(tab => chrome.tabs.sendMessage(tab.id, message, resolve));
+	});
+}
+
+function getActiveTab() {
+	return new Promise((resolve, reject) => {
+		chrome.tabs.query({ 'active': true, 'currentWindow': true }, tabs => resolve(tabs.shift()) );
+	});
+}
