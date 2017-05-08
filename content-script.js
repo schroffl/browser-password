0848dcb700d5a7948e84c936201b313c56259a5d
schroffl <Hannes.Holzwarth@me.com>
2017-05-08 14:31:59 +0200

    Renamed to content-scripts/form.js

--- content-script.js
+++
@@ -1,9 +0,0 @@
-console.log('asdljwer');
-
-window.addEventListener('load', function() { 
-	let forms = Array.from(document.forms),
-   		formElements = forms.map(form => Array.from(form.elements)),
-		elements = Array.prototype.concat.apply([ ], formElements);
-						
-	console.log(elements);
-}, false);
