d4e4a745616cce54cb9bde8ea485531dca6eb170
schroffl <Hannes.Holzwarth@me.com>
2017-05-08 14:12:51 +0200

    Use Grunt to build the unsigned and signed .zip-files for the extension

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = function(grunt) {
+	grunt.initConfig({
+		'less': {
+			'production': {
+				'files': {
+					'ui/css/popup.css': 'ui/less/popup.less'
+				}
+			}
+		},
+		'crx': {
+			'publicExtension': {
+				'src': [ 'manifest.json', 'content-script.js', 'background/**', 'ui/**', '!ui/less'  ],
+				'dest': 'dist/public.zip'
+			},
+			'signedExtension': {
+				'src': [ 'manifest.json', 'content-script.js', 'background/**', 'ui/**', '!ui/less' ],
+				'dest': 'dist/signed.crx',
+				'options': {
+					'privateKey': 'private_key.pem'
+				}
+			}
+		}
+	});
+	
+
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-crx');
+
+	grunt.registerTask('default', [ 'less', 'crx' ]);
+};
