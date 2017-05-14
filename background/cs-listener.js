c7ed6d74b223f4cfb0e8d15c56ba561ee236aefe
Hannes <Hannes.Holzwarth@me.com>
2017-05-14 14:46:48 +0200

    A domain matches, if it contains every part of the tray-domain and they are in the same position

--- background/cs-listener.js
+++ background/cs-listener.js
@@ -1,8 +1,23 @@
+function compareDomain(domain, match) {
+	let domainArr = domain.split('.').reverse(),
+		matchArr = match.split('.').reverse();
+
+	if(matchArr.length > domainArr.length)
+		return false;
+
+	for(let i=0; i<matchArr.length; i++) {
+		if(matchArr[i] !== domainArr[i])
+			return false;
+	}
+
+	return true;
+}
+
 chrome.runtime.onMessage.addListener((req, sender, respond) => {
 	if(sender.tab && req.action === 'get') {
 		let domain = req.domain,
 			entries = window.vault.entries,
-			entry = entries.find(entry => entry.props.other.domain === domain);
+			entry = entries.find(entry => compareDomain(domain, entry.props.other.domain));
 
 		respond(entry);
 	}
